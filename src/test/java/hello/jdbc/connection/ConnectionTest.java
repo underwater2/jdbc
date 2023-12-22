ab3010290fd787c6f038663dce24bab5d8a598f1
underwater2 <shaian2@naver.com>
2023-12-22 09:28:58 +0900

    완강

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,54 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("Connection={}, Class={}", con1, con1.getClass());
+        log.info("Connection={}, Class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션 획득
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000); //커넥션 풀에서 커넥션이 생성하는 동안(별도 쓰레드에서 동작) 메인 스레드가 계속 실행되도록 유지한다.
+    }
+
+    private static void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("Connection={}, Class={}", con1, con1.getClass());
+        log.info("Connection={}, Class={}", con2, con2.getClass());
+    }
+
+}
